2d44ea002823fec1b38764133a0294876387a52e
kaulketh <kaulketh@gmail.com>
2026-08-01 18:50:06 +0200

    Connect camera capture to application entry point

--- src/birdpi/main.py
+++ src/birdpi/main.py
@@ -1,5 +1,23 @@
+"""
+The script initializes the BirdPi system, interfaces with a camera to
+capture an image, and outputs relevant status and results.
+
+This module is responsible for setting up and interacting with the
+camera component of the BirdPi system to capture an image and save it
+to a configured directory. It serves as the entry point for the BirdPi
+system's functionality.
+"""
+
+from birdpi.camera.capture import Camera
+from birdpi.config import IMAGE_PATH
+
+
 def main():
     print("🐦 BirdPi online")
+
+    camera = Camera(IMAGE_PATH)
+    image = camera.capture()
+    print(f"📸 Image captured: {image}")
 
 
 if __name__ == "__main__":
