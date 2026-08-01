63cde9650c85ae47e4c252a009edc900fc64aebf
kaulketh <kaulketh@gmail.com>
2026-08-01 18:24:54 +0200

    Add BirdPi application entry point

---
+++ src/birdpi/__main__.py
@@ -0,0 +1,6 @@
+def main():
+    print("🐦 BirdPi online")
+
+
+if __name__ == "__main__":
+    main()
--- src/birdpi/config.py
+++ src/birdpi/config.py
@@ -1,4 +1,10 @@
-IMAGE_PATH = "/home/kaulketh/birdpi-data/images"
+from pathlib import Path
+
+DATA_PATH = Path(
+    "/home/kaulketh/birdpi-data"
+)
+
+IMAGE_PATH = DATA_PATH / "images"
 
 CAMERA_WIDTH = 1920
 CAMERA_HEIGHT = 1080
